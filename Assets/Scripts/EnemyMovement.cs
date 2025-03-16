8947ef5432b2ad71bdbf56805b03e5b683168379
Zaif K <159206675+Zaif-K@users.noreply.github.com>
2025-03-17 09:39:51 +1100

    did work on cs and functionality

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,23 +5,28 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
+ // Reference to the player's transform.
+ public Transform player;
 
-public Transform player;
-private NavMeshAgent navMeshAgent;
+ // Reference to the NavMeshAgent component for pathfinding.
+ private NavMeshAgent navMeshAgent;
 
-
-    // Start is called before the first frame update
-    void Start()
+ // Start is called before the first frame update.
+ void Start()
     {
+ // Get and store the NavMeshAgent component attached to this object.
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+ // Update is called once per frame.
+ void Update()
     {
-              if (player != null)
-       {    
-           navMeshAgent.SetDestination(player.position);
-       } 
+ // If there's a reference to the player...
+ if (player != null)
+        {    
+ // Set the enemy's destination to the player's current position.
+            navMeshAgent.SetDestination(player.position);
+        }
     }
 }
+
